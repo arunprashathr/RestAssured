335d5656f612746fced3f925d2947a3df537ed9b
arunprashathr <104611989+arunprashathr@users.noreply.github.com>
2022-05-06 12:46:20 +0530

    Add files via upload

---
+++ src/test/java/RunnerPackage/RunnerClass.java
@@ -0,0 +1,21 @@
+package RunnerPackage;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( features = "src/test/resources/Feature", glue = { "stepDefinition" }, monochrome = true,
+
+//tags = "@newUser",
+//tags = "@user",
+//tags = "@login",
+
+plugin = { "pretty", "html:target/reports/index.html", 
+		   "pretty", "json:target/reports/cucumber.json",
+           "pretty", "junit:target/reports/cucumber.xml",
+           "pretty", "testng:target/reports/cucumber.xml" } )
+
+public class RunnerClass {
+
+}
